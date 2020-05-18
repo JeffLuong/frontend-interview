4d227d3b7134fb10e7c83cfe326c18169f8ef42a
Jeffrey Luong <jjck1988@gmail.com>
2020-05-18 13:42:14 -0400

    Add feature to update profile name

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,35 +1,39 @@
-import * as React from "react";
-import { useUserContext } from "../contexts/user";
+import * as React from 'react';
+import { useUserContext, useSetUserContext } from '../contexts/user';
+import useFormValues from '../hooks/useFormValues';
 
 /**
  * This should update the user context with the new values for email and name
  */
 const Profile = () => {
   const user = useUserContext();
+  const setUserContext = useSetUserContext();
+  const [updatedUser, setUpdatedUser] = useFormValues(user);
+  const onSubmit = e => {
+    if (updatedUser.email && updatedUser.name) {
+      setUserContext({
+        ...user,
+        ...updatedUser
+      });
+    }
+    e.preventDefault();
+  };
 
   return (
     <div>
       <h1>Edit your profile</h1>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-        }}
-      >
+      <form onSubmit={onSubmit}>
         <input
-          name="email"
-          value={user.email}
-          onChange={e => {
-            user.email = e.target.value;
-          }}
+          name='email'
+          value={updatedUser.email}
+          onChange={setUpdatedUser}
         />
         <input
-          name="name"
-          value={user.name}
-          onChange={e => {
-            user.name = e.target.value;
-          }}
+          name='name'
+          value={updatedUser.name}
+          onChange={setUpdatedUser}
         />
-        <button type="submit">Submit</button>
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
